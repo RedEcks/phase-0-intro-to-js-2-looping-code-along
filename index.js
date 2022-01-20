797c44fdc4ab2faac692cf9234a5b97d8c516f0d
Xavier Dunkley <xavierxv18@gmail.com>
2022-01-20 12:04:19 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+const gifts = ["teddy bear","drone","doll"];
+
+function wrapGifts(gifts){
+    for (let i=0;i<gifts.length;i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+wrapGifts(gifts);
+
+
+function writeCards([name],eventName){
+    let names=[];
+    let message=[];
+    names.push(name);
+    for(let n=1;n<names.length;n++){
+        message.push(`Thank you, ${name}, for the wonderful ${eventName} gift!`);
+    }
+    return message;
+}
+writeCards(names, eventName);
+
+function countDown(){
+    let number=10;
+    while(number>=0){
+        console.log(number--);
+    }
+}
